9f0e9681cf604cb73271f4d3453b7fb88fc81ebb
Brian <riceb53@gmail.com>
2019-12-29 11:37:00 -0600

    change machine outline

---
+++ change_machine.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+
+# cents
+
+
+RSpec.describe ChangeMachine do
+  describe '#change' do
+    it 'should return [1] when given 1' do
+      machine = ChangeMachine.new
+      expect(machine.change(1)).to eq([1])
+    end
+
+    it 'should return [10,10,1,1,1] when given 23' do
+      machine = ChangeMachine.new
+      expect(machine.change(23)).to eq([10,10,1,1,1])
+    end
+  end
+end
